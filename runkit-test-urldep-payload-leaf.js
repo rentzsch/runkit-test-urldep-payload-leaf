9df0eac15941d12fa604911180df3cd58f4ebfce
rentzsch <jwr.git@redshed.net>
2017-03-15 10:59:17 -0700

    fix version reporting

--- runkit-test-urldep-payload-leaf.js
+++ runkit-test-urldep-payload-leaf.js
@@ -1,9 +1,11 @@
 const assert = require("assert");
 
-const lodashIsNullPackage = require("lodash.isnull/package.json").version;
-assert.deepStrictEqual(lodashIsNullPackage, "2.0.0");
+const thisPackageVersion = require("./package.json").version;
+
+const lodashIsNullPackageVersion = require("lodash.isnull/package.json").version;
+assert.deepStrictEqual(lodashIsNullPackageVersion, "2.0.0");
 
 module.exports = {
   name: "runkit-test-urldep-payload-leaf",
-  version: "1.0.0"
+  version: thisPackageVersion
 };
